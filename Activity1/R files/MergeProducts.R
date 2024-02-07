78cafcd1146b5bb20f6e80c8b85b40eb9e505316
Carl-Jason-Omisol <carljason.omisol@students.isatu.edu.ph>
2024-02-07 04:16:35 +0000

    1st try

---
+++ Activity1/R files/MergeProducts.R
@@ -0,0 +1,8 @@
+
+product1 <- read.csv("/cloud/project/Activity1/CSV files/First_product.csv")
+product2 <- read.csv("/cloud/project/Activity1/CSV files/Seccond_product.csv")
+product3 <- read.csv("/cloud/project/Activity1/CSV files/Third_product.csv")
+
+merged_products <- rbind(product1, product2, product3)
+
+write.csv(merged_products, "MergedProducts.csv", row.names = FALSE)
